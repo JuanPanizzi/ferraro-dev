b5875df555cbc1782938693c83973a61b0047431
ales0sa <alesosa@gmail.com>
2024-08-17 23:52:55 -0300

    GO

--- src/service/ArticleService.js
+++ src/service/ArticleService.js
@@ -3,14 +3,13 @@ import { API_BASE_URL } from './config';
 export const ArticleService = {
     async searchArticle(code) {
         // post
-        const response = await fetch(`${API_BASE_URL}/articles_search`, {
-            method: 'POST',
+        const response = await fetch(`${API_BASE_URL}/articulos/search?code=${code}`, {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ code: code })
+            }
         });
-
+        
         return await response.json();
     }
 };
